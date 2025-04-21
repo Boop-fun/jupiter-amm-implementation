75d2b48d4ebd9722415cf370c054f5b2227e2825
Boopie <boopie@boop.fun>
2025-04-21 09:43:54 +0100

    feat: Key

--- jupiter-core/src/amms/boop_fun_amm.rs
+++ jupiter-core/src/amms/boop_fun_amm.rs
@@ -1,17 +1,18 @@
 use anyhow::Result;
-use jupiter_amm_interface::{AmmContext, KeyedAccount};
+use jupiter_amm_interface::{Amm, AmmContext, KeyedAccount};
 use solana_sdk::pubkey;
 use solana_sdk::pubkey::Pubkey;
 
 pub const BOOP_FUN_PROGRAM: Pubkey = pubkey!("boop8hVGQGqehUK2iVEMEnMrL5RbjywRzHKBmBE7ry4");
 
 pub struct BoopFunAmm {
+    key: Pubkey,
 }
 
 // TODO: impl Amm for BoopFunAmm
 impl BoopFunAmm {
     fn from_keyed_account(keyed_account: &KeyedAccount, amm_context: &AmmContext) -> Result<Self> {
-        Ok(BoopFunAmm { })
+        Ok(BoopFunAmm { key: keyed_account.key })
     }
 
     fn label(&self) -> String {
@@ -20,5 +21,9 @@ impl BoopFunAmm {
 
     fn program_id(&self) -> Pubkey {
         BOOP_FUN_PROGRAM
+    }
+
+    fn key(&self) -> Pubkey {
+        self.key
     }
 }
